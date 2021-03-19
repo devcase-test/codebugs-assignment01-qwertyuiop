a4117b1dfc958903596d62d8aedeb7bd9fbaeac2
Sytac Dev <31990977+sytac-bot@users.noreply.github.com>
2021-03-19 10:04:30 +0100

    Initial devcase commit

---
+++ task-api/index.ts
@@ -0,0 +1,11 @@
+import { getJoeFriends, getJoeFriendDetails } from './rest';
+
+export const run = () => {
+    throw new Error('Not implemented yet');
+};
+
+try {
+    run();
+} catch (error) {
+    console.error(error);
+}
