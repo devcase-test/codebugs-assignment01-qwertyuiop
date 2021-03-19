7ca2b7f76def4e3f10cbbd97941a11cbe6c307b8
Sytac Dev <31990977+sytac-bot@users.noreply.github.com>
2021-03-19 10:04:32 +0100

    Initial devcase commit

---
+++ task-api/mocks.ts
@@ -0,0 +1,38 @@
+export const JoeFriends = [
+    {
+        id: '97f4a7ba-2027-11eb-adc1-0242ac120002',
+        name: 'Jane',
+        surname: 'Doe',
+    },
+    {
+        id: '9eb39408-2027-11eb-adc1-0242ac120002',
+        name: 'Tim',
+        surname: 'Doe',
+    },
+    {
+        id: 'a6217606-2027-11eb-adc1-0242ac120002',
+        name: 'Bil',
+        surname: 'Goe',
+    },
+];
+
+export const JoeFriendDetails = [
+    {
+        id: '97f4a7ba-2027-11eb-adc1-0242ac120002',
+        name: 'Jane',
+        surname: 'Doe',
+        friendsCount: 12,
+    },
+    {
+        id: '9eb39408-2027-11eb-adc1-0242ac120002',
+        name: 'Tim',
+        surname: 'Doe',
+        friendsCount: 14,
+    },
+    {
+        id: 'a6217606-2027-11eb-adc1-0242ac120002',
+        name: 'Bil',
+        surname: 'Goe',
+        friendsCount: 15,
+    },
+];
